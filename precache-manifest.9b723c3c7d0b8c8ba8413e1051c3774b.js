3fa4116dcfe3aff9022d93c52233e243b07d62a2
Daniil <daniel.eshenko@gmail.com>
2020-09-24 17:42:50 +0300

    Updates

---
+++ precache-manifest.9b723c3c7d0b8c8ba8413e1051c3774b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ad0c84ba5f7db2ccd7c188dc266c4ca",
+    "url": "/index.html"
+  },
+  {
+    "revision": "05cfd6873761dfb8e060",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "1d1ba8b0ba51dcc2dfeb",
+    "url": "/static/css/main.55892344.chunk.css"
+  },
+  {
+    "revision": "05cfd6873761dfb8e060",
+    "url": "/static/js/2.14c64bd6.chunk.js"
+  },
+  {
+    "revision": "229313a91dfc1a4229b4758368bd8914",
+    "url": "/static/js/2.14c64bd6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d1ba8b0ba51dcc2dfeb",
+    "url": "/static/js/main.0d66e44e.chunk.js"
+  },
+  {
+    "revision": "9026812eecb507bc641b",
+    "url": "/static/js/runtime-main.b02c64c9.js"
+  }
+]);
